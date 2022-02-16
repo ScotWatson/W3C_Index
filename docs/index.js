2c94952af3a5977ba4584a3ecd0edc8d82c09c6a
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-02-15 22:02:04 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -4,10 +4,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 */
 
 window.addEventListener("load", function (evt) {
-  console.log("load");
   const xmlFetch = fetch("W3C.xml");
   const xmlText = xmlFetch.then(function (response) {
-    console.log("read");
     console.log(response);
     return response.text();
   });
